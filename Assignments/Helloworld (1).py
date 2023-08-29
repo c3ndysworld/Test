ed38c6619b22ecbc7b94e1043929f495b2156e53
c3ndysworld <143369475+c3ndysworld@users.noreply.github.com>
2023-08-28 23:01:05 -0500

    Add files via upload

---
+++ Assignments/Helloworld (1).py
@@ -0,0 +1,20 @@
+# Full Name: Cendy Sanchez
+# Student ID: 0903681
+# Assignment Due Date: 8-27-2023
+# MSITM 6341: FA01
+# Assignment #1
+# Assignment Title: Hello World
+# Helloworld.py
+
+
+# any statement that start with # symbol will be considered as non-executable or comment line
+# please create the python file with an extension of .py file
+# the assignment submission file must be a .py (python file) 
+# open visual studio code and click new file menu and type type file name as HelloWorld.py 
+# save the file and add necessary assignment specific print statement and run the application.
+# before submitting the assignment make sure that you are getting expected output.
+ 
+print ("Hello World!")
+print ("My name is Cendy Sanchez")
+print ("I am a python student")
+print ("Have a nice day!")
